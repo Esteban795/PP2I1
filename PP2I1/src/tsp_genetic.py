055f22bbba4cf08a8999e8d8dc19b865cf45f4c0
Esteban MANDJUKOWSKI <estebanmandjukowski@gmail.com>
2023-11-04 15:29:20 +0100

    feat : [~] reworking genetic algorithm because it runs like shit

--- PP2I1/src/tsp_genetic.py
+++ PP2I1/src/tsp_genetic.py
@@ -1,189 +1,188 @@
 import numpy as np
-import numpy.typing as npt
+
 import matplotlib.pyplot as plt
 import tsplib95
+from typing import Callable
 
+#City_List,size_population=1000,elite_size=75,mutation_Rate=0.01,generation=1000,num_selected=500
 class TSPSolver:
-    """Class to instantiate a TSP solver, through a genetic algorithm.
-    It allows you to use your own distance function, choose the population size and the mutation rate.
-    """
-    def __init__(self,coordinates_list : list[tuple[float,float]] ,n_population : int, mutation_rate : float,dist_function : callable):
-        self.n_cities = len(coordinates_list)
-        self.n_population = n_population
-        self.mutation_rate = mutation_rate
-        self.coordinates_list = coordinates_list
-        self.dist_function = dist_function
-        self.names_list = np.array([i for i in range(1,self.n_cities + 1)])
-        self.cities_dict = {x:y for x,y in zip(self.names_list,coordinates_list)}
-        
-    def computeDistance(self,city_a : int, city_b : int) -> float:
-        """Computes the distance between two cities
-        Args:
-            city_a (int): City A
-            city_b (int): City B
-        Returns:
-            float: Distance between city A and city B
-        """
-        return self.dist_function(self.cities_dict[city_a], self.cities_dict[city_b])
 
-    def genesis(self, cities_list : list[int]) -> npt.NDArray:
+    def __init__(self,coords : list[tuple[float,float]], pop_size : int, dist_function : Callable,mutation_rate : float = 0.01,elite_size : int = 75) -> None:
+        self.coords = coords
+        self.pop_size = pop_size
+        self.mutation_rate = mutation_rate
+        self.dist_function = dist_function
+        self.elite_size = elite_size
+    
+    def generateInitPopulation(self) -> list[list[int]]:
         """
         Generates the initial population of the genetic algorithm
-        Args:
-            cities_list : List of cities to be visited
         Returns:
             npt.NDArray: Initial population 
         """
-        return np.array([cities_list[np.random.permutation(self.n_cities)] for i in range(self.n_population)])
+        return [np.random.permutation(len(self.coords)) for i in range(self.pop_size)]
+
+    def getDistance(self, city_1 : tuple[int,int], city_2 : tuple[int,int]) -> float:
+        """
+        Calculates the distance between two cities
+        Args:
+            city_1 (tuple[int,int]): First city
+            city_2 (tuple[int,int]): Second city
+        Returns:
+            float: Distance between the two cities
+        """
+        return self.dist_function(city_1,city_2)
     
-    def evalPermutationFitness(self, city_list : npt.ArrayLike) -> float:
+    def getPopulationScore(self,population : list[list[int]]) -> list[float]:  
         """
-        Evaluates the fitness of a permutation of cities
+        Calculates the fitness score for each individual in the population
         Args:
-            city_list : List of cities to be visited
+            population (list[list[int]]): Population to calculate the fitness scores of
         Returns:
-            float: Fitness of the permutation
+            list[float]: Fitness scores of the population
         """
-        total = 0
-        for i in range(self.n_cities - 1):
-            a = city_list[i]
-            b = city_list[i + 1]
-            total += self.computeDistance(a,b)
-        return total
+        return [self.getFitness(route) for route in population]
     
-    def getAllFitness(self, population_set : npt.NDArray) -> npt.NDArray:
+    def getFitness(self,route):
         """
-        Gets the fitness of all the permutations in the population
+        Calculates the fitness of a route
         Args:
-            population_set : Population of permutations
+            route : Route to calculate the fitness of
         Returns:
-            fitness_list : Fitness of all the permutations
+            float: Fitness of the route
         """
-        fitness_list = np.zeros(self.n_population)
-        for i in range(self.n_population):
-            fitness_list[i] = self.evalPermutationFitness(population_set[i])
-        return fitness_list
+        fitness_score = 0
+        for i in range(len(route)):
+            j = (i + 1) % len(route)
+            fitness_score += self.getDistance(self.coords[route[i]],self.coords[route[j]])
+        return fitness_score
     
-    def selectProgenitor(self, population_set : npt.NDArray, fitness_list : npt.ArrayLike) -> npt.ArrayLike:
+    def makeCrossover(self, parent_1 : list[int], parent_2 : list[int]) -> list[int]:
         """
-        Selects two progenitors from the population that best minimize the sum of the distances between the cities
+        Makes a crossover between two parents
         Args:
-            population_set : Population of permutations
-            fitness_list : Fitness of all the permutations
+            parent_1 : First parent
+            parent_2 : Second parent
         Returns:
-            progenitor_list : List of progenitors
+            list[int]: Child route
         """
-        total_fit = fitness_list.sum()
-        prob_list = fitness_list/total_fit
-        # A progenitor can mate with itself, so we can't use a permutation (duplicates not allowed)
-        progenitor_list_a = np.random.choice(range(len(population_set)), len(population_set),p=prob_list, replace=True)
-        progenitor_list_b = np.random.choice(range(len(population_set)), len(population_set),p=prob_list, replace=True)
-        progenitor_list_a = population_set[progenitor_list_a]
-        progenitor_list_b = population_set[progenitor_list_b]
-        return np.array([progenitor_list_a,progenitor_list_b])
+        childP1 = [0 for i in range(len(parent_1))]
+        geneA = int(np.random.random() * len(parent_1))
+        geneB = int(np.random.random() * len(parent_1))
+        startGene = min(geneA, geneB)
+        endGene = max(geneA, geneB)
+        for i in range(startGene, endGene):
+            childP1.append(parent_1[i])
+        childP2 = [item for item in parent_2 if item not in childP1]
+        return childP1 + childP2
 
-    def mateProgenitors(self, prog_a : npt.ArrayLike, prog_b : npt.ArrayLike) -> npt.ArrayLike:
+    #Centre Inverse Mutation (CIM)
+    def applyCIM(self,route : list[int]) -> list[int]:
         """
-        Mates two progenitors, generating an offspring
+        Applies the CIM (Centre Inverse Mutation) mutation to a route
         Args:
-            prog_a : Progenitor A
-            prog_b : Progenitor B
+            route : Route to apply the mutation to
         Returns:
-            offspring : offspring of the two progenitors
+            list[int]: Route after the mutation
         """
-        offspring = prog_a[0:5]
-        for city in prog_b:
-            if not city in offspring:
-                offspring = np.concatenate((offspring,[city]))
-        return offspring
-
-    def matePopulation(self, progenitor_list : npt.ArrayLike) -> npt.NDArray:
-        """
-        Mates the population of progenitors, generating a new population
-        Args:
-            progenitor_list : List of progenitors
-        Returns:
-            new_population_set : newly generated population
-        """
-        new_population_set = []
-        for i in range(progenitor_list.shape[1]):
-            prog_a, prog_b = progenitor_list[0][i], progenitor_list[1][i]
-            offspring = self.mateProgenitors(prog_a, prog_b)
-            new_population_set.append(offspring)
-        return new_population_set
+        index = len(route) // 2
+        first_half = route[:index][::-1]
+        second_half = route[index:][::-1]
+        return first_half + second_half
     
-    def mutateOffspring(self, offspring : npt.ArrayLike) -> npt.ArrayLike:
+    def selectBestResults(self, population_ranked : list[int]) -> list[list[int]]:
         """
-        Randomly mutates a certain number of cities in the offspring depending on the mutation rate.
-        Args:
-            offspring : Offspring to be mutated
+        Selects the best results from a ranked population
+        Args:s
+            population_ranked: Ranked population 
         Returns:
-            offspring : Mutated offspring
+            list[list[int]]: Best results from the ranked population
         """
-        for i in range(int(self.n_cities * self.mutation_rate)):
-            a = np.random.randint(0,self.n_cities)
-            b = np.random.randint(0,self.n_cities)
-            offspring[a], offspring[b] = offspring[b], offspring[a]
-        return offspring
+        return [population_ranked[i][0] for i in range(self.elite_size)]
     
-    def mutatePopulation(self, new_population_set : npt.ArrayLike) -> npt.NDArray:
+    def sortElitistRoutes(self, population : list[list[int]]) -> list[list[int]]:
         """
-        Randomly mutates the population set.
+        Sorts the population by elitism (best fitness score)
         Args:
-            new_population_set : Population to be mutated
+            population : Population to sort
         Returns:
-            mutated_pop : Mutated population
+            list[(i,fitness_score)]: Sorted population
         """
-        mutated_pop = []
-        for offspring in new_population_set:
-            mutated_pop.append(self.mutateOffspring(offspring))
-        return mutated_pop
+        return sorted([(i,self.getFitness(population[i])) for i in range(len(population))],key=lambda x: x[1],reverse=False)
     
-    def solve(self,n_generation : int) -> list[int,float,npt.ArrayLike]:
+    def breedPopulation(self, mating_pool : list[list[int]]) -> list[list[int]]:
         """
-        TSP solver using a genetic algorithm approach.
+        breeds the population together.
         Args:
-            n_generation : Number of generations to be run (the more the better for precision, but the slower the algorithm)
-        Returns:
-            best_solution : Best solution found by the algorithm
+            mating_pool : Mating pool
         """
-        #initialization phase
-        population_set = self.genesis(self.names_list)
-        fitness_list = self.getAllFitness(population_set)
-        progenitor_list = self.selectProgenitor(population_set,fitness_list)
-        new_population_set = self.matePopulation(progenitor_list)
-        mutated_pop = self.mutatePopulation(new_population_set)
-        best_solution = [-1,np.inf,np.array([])]
-        for i in range(n_generation - 1):
-            fitness_list = self.getAllFitness(population_set)
-            if fitness_list.min() < best_solution[1]:
-                best_solution[0] = i
-                best_solution[1] = fitness_list.min()
-                best_solution[2] = np.array(mutated_pop)[fitness_list.min() == fitness_list]
-            progenitor_list = self.selectProgenitor(population_set,fitness_list)
-            new_population_set = self.matePopulation(progenitor_list)
-            mutated_pop = self.mutatePopulation(new_population_set)
-        return best_solution
+        return [self.makeCrossover(mating_pool[i],mating_pool[i + 1]) for i in range(len(mating_pool) - 1)]
 
-def distance_function(city_1 : list[int,int],city_2 : list[int,int]) -> float:
-    return ((city_1[0] - city_2[0])**2 + (city_1[1] - city_2[1])**2)**0.5
+    def mutatePopulation(self,children : list[list[int]]) -> list[list[int]]:
+        """
+        mutate the population by applying the CIM mutation
+        Args :
+            children : children
+        Returns : 
+            list[list[int]] : a list of the mutated children
+        """
+        return [self.applyCIM(children[i]) for i in range(10)]
 
-data = tsplib95.load('berlin52.tsp')
-cities = list(data.get_nodes())
-coords = list(data.node_coords.values())
+    def matingPool(self, population : list[list[int]], selection_results : list[int]) -> list[list[int]]:
+        """
+        Creates a mating pool from a population
+        Args:
+            population : Population to create the mating pool from
+            selection_results : Selection results
+        Returns:
+            list[list[int]]: Mating pool
+        """
+        return [population[selection_results[i]] for i in range(len(selection_results))]
 
-tsp = TSPSolver(coords,1000,0.3,distance_function)
+    def generateNextGeneration(self, population : list[list[int]]) -> list[list[int]]:
+        """
+        Generates the next generation
+        Args:
+            population : Population to generate the next generation from
+        Returns:
+            list[list[int]]: Next generation
+        """
+        population_ranked = self.sortElitistRoutes(population)
+        selection_results = self.selectBestResults(population_ranked)
+        mating_pool = self.matingPool(population,selection_results)
+        children = self.breedPopulation(mating_pool)
+        next_generation = self.mutatePopulation(children)
+        del children[:10]
+        children.extend(next_generation)
+        return children
 
-xs = [coord[0] for coord in coords]
-ys = [coord[1] for coord in coords]
-plt.scatter(xs,ys)
+    def solve(self,n_generations : int) -> list[list[int]]:
+        """
+        Solves the TSP problem
+        Args:
+            n_generations (int): Number of generations to run the algorithm for
+        Returns:
+            list[list[int]]: Best route found by the algorithm
+        """
+        population = self.generateInitPopulation()
+        for i in range(n_generations):
+            pop = self.generateNextGeneration(population)
+            population.extend(pop)
+            del population[:self.elite_size]
+            print("Generation=",i)         
+        ranked = sorted(self.sortElitistRoutes(pop))[0]
+        print("RANK_",ranked)
+        print(f"Best Route :{population[ranked[0]]} ")
+        print(f"best route distance {ranked[1]}")
+        plt.ylabel('Distance')
+        plt.xlabel('Generation')
+        plt.show()
+        return ranked, pop
 
-res = tsp.solve(1000)
-arr = res[2]
 
-xs = [coords[i-1][0] for i in arr[0]]
-ys = [coords[i-1][1] for i in arr[0]]
-plt.plot(xs,ys)
+cityList= [(33, 89), (9, 81), (40, 126), (80, 96), (120, 124), (127, 110), (93, 67), (89, 140), (186, 104), (136, 192), (131, 169), (179, 181), (120, 7), (184, 130), (111, 185), (120, 187), (110, 193), (1, 27), (68, 5), (194, 173), (139, 52), (97, 74), (198, 11), (164, 60), (45, 145)]
 
+tsp = TSPSolver(cityList,1000,lambda x,y : np.sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2))
+
+res = tsp.solve(300)
 print(res)
-plt.show()
