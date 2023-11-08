e3fe1a279e41f2aa034c79be3f5e5f5a908516f8
moise7000 <77933570+moise7000@users.noreply.github.com>
2023-11-08 09:10:02 +0100

    Update PickUp.py
    add the serialize function to add in the DB easily

--- PP2I1/src/PickUp.py
+++ PP2I1/src/PickUp.py
@@ -2,9 +2,16 @@ from datetime import datetime as dt
 
 
 class PickUp:
-    def __init__(self, truck_id, bin_id):
+    def __init__(self,
+                 truck_id: int,
+                 bin_id: int):
         self.truck_id = truck_id
         self.bin_id = bin_id
         self.pickup_time = dt.timestamp(dt.now())
 
 
+    def serialize(self):
+        '''Méthode de formatage pour ajouter les données dans la DB'''
+        return (self.truck_id,
+                self.bin_id,
+                self.pickup_time)
