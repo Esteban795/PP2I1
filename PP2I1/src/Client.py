d1d759d4577cde4e5cb03b0c6fc70059b80cd941
moise7000 <77933570+moise7000@users.noreply.github.com>
2023-11-08 09:08:56 +0100

    Update Client.py
    add the serialize function to add in DB easily

--- PP2I1/src/Client.py
+++ PP2I1/src/Client.py
@@ -1,14 +1,30 @@
 from datetime import datetime as dt
 
 class Client:
-    def __init__(self, first_name, last_name, email, password):
+    def __init__(self,
+                 client_id: int,
+                 first_name: str,
+                 last_name: str,
+                 email: str,
+                 password: str):
+        self.client_id = client_id
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.password = password
-        self.created = dt.timestamp(dt.now())
+        self.created_at = dt.timestamp(dt.now())
         self.recycled_volume = 0
 
 
     def __str__(self):
         return "{0} {1}".format(self.first_name, self.last_name)
+
+    def serialize(self):
+        '''Méthode de formatage pour ajouter les données dans la DB'''
+        return (self.client_id,
+                self.first_name,
+                self.last_name,
+                self.email,
+                self.created_at,
+                self.password,
+                self.recycled_volume)
