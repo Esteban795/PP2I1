8c842f02a60c6561eb665ad07745baddc750a614
Ewan Decima <ewan.decima@telecomnancy.eu>
2024-01-15 20:06:42 +0100

    correction de la requete sql dans delete_user()

--- PP2I1/src/backend/app.py
+++ PP2I1/src/backend/app.py
@@ -503,9 +503,10 @@ def delete_user():
         confirm_password = hash_object.hexdigest()
         if current_user.password != confirm_password:
             return render_template('profile.html',error_deleteuser="Le mot de passe est incorrect.",user=current_user, recycled_volume=10, total_volume=20, recycled_percentage=50)
-        cursor.execute('DELETE clients WHERE client_id = ?', (current_user_id,))
-        cursor.execute('DELETE bins WHERE owner_id = ?', (current_user_id,))
-        #cursor.execute('UPDATE clients SET first_name = ?, last_name = ?, email = ?, created_at = ?, pwd = ?, recycled_volume = ?, status = ? WHERE client_id = ?', (None, None, None, None, None,None, None, current_user_id))
+        cursor.execute('DELETE FROM clients WHERE client_id = ?', (current_user_id,))
+        
+        cursor.execute('DELETE  FROM bins WHERE owner_id = ?', (current_user_id,))
+
         conn.commit()
         return redirect(url_for('login'))
     return redirect(url_for('profile'))
@@ -572,4 +573,4 @@ def reset_email():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=8081)
